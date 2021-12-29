862c467e6e1a69b26dfa97ef1cf73b06ea726015
pthomas0309 <preston.thomas355@gmail.com>
2021-12-28 19:57:57 -0600

    Use string methods to add and remove whitespace from strings

--- names.py
+++ names.py
@@ -24,8 +24,52 @@ last_name = "granger"
 # with the value of the variable.
 full_name = f"{first_name} {last_name}"
 
+# f-string syntax for Python 3.5 and earlier
+# full_name = "{} {}".format(first_name. last_namei8)
+
 # Output - hermoine granger
 print(full_name)
 
-# Output - Hello, Hermoine Granger
+# Output - Hello, Hermoine Granger!
 print(f"Hello, {full_name.title()}!")
+
+# Make the greeting its own variable
+greeting = f"Hello, {full_name.title()}!"
+
+# Output - Hello, Hermoine Granger!
+print(greeting)
+
+# Whitespace: Tabs and Newlines
+
+# \t denotes a tab
+# Output -      Python
+print("\tPython")
+
+# \n denotes a newline
+# Output - 
+# Desserts:
+# Creme Brulee
+# Chocolate Lava Cake
+# Sugar Doughnuts
+print("Desserts:\nCreme Brulee\nChocolate Lava Cake\nSugar Doughnuts")
+
+# Output - 
+# Desserts:
+#   Creme Brulee
+#   Chocolate Lava Cake
+#   Sugar Doughnuts
+print("Desserts:\n\tCreme Brulee\n\tChocolate Lava Cake\n\tSugar Doughnuts")
+
+favorite_language = "  python "
+
+# rstrip method removes whitespace from the right side of the string
+# Output - "  python"
+print(favorite_language.rstrip())
+
+# lstrip method removes whitespace from the left side of the string
+# Output - "python "
+print(favorite_language.lstrip())
+
+# strip method removes whitespace from both sides of the string
+# Output - "python"
+print(favorite_language.strip())
