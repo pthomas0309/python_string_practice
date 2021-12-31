21e4f0de8445c8e3a15282ef122972a96859a340
pthomas0309 <preston.thomas355@gmail.com>
2021-12-31 00:21:14 -0600

    Use integers to perform mathmatical operations

---
+++ numbers/integer_operations.py
@@ -0,0 +1,30 @@
+# The + operator represents addition
+# Output - 8
+print(2 + 6)
+
+# The - operator represents subtraction
+# Underscores helps to break up larger numbers and does not effect the outcome of the operation
+# Output - 8
+print(1_000 - 992)
+
+# Doubling the multiplication operator (*) represents exponents
+# Output - 8
+print(2 ** 3)
+
+# The / operator represents division
+# Output - 8.0
+print(64 / 8)
+
+# The * operator represents multiplication
+# Output - 8
+print(1 * 8)
+
+# Python does not have a data type to handle constants, 
+# variable names in all caps are meant to be treated as constants.
+# Creating and assigning variables in one line is accomplished by seperating the variable
+# names and their corresponding values by commas.
+FAVORITE_NUMBER, LEAST_FAVORITE_NUMBER = 7, 15
+
+# Output - 7 is the best number, and the worst by far is 15
+print(f"{FAVORITE_NUMBER} is the best number, and the worst by far is {LEAST_FAVORITE_NUMBER}")
+
